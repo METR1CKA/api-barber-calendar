27b57ec9f85c0a1deef7f7f4f390d747bae2109a
METR1CKA <ferchosalazar054@gmail.com>
2024-11-23 18:00:22 -0600

    feat: entity and dto from schedules updated

--- src/modules/schedules/dto/create-schedule.dto.ts
+++ src/modules/schedules/dto/create-schedule.dto.ts
@@ -1,1 +1,77 @@
-export class CreateScheduleDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator'
+import { IsValidTime } from 'src/core/decorators/is-valid-time.decorator'
+import { FORMATS } from 'src/shared/utils/luxon-datetime'
+
+export class CreateScheduleDto {
+    @IsNotEmpty({
+        message: 'El id del usuario barbero es requerido',
+    })
+    @IsNumber(
+        {},
+        {
+            message: 'El id del usuario barbero debe ser un numero',
+        },
+    )
+    @IsPositive({
+        message: 'El id del usuario barbero debe ser un numero positivo',
+    })
+    public user_barber_id: number
+
+    @IsNotEmpty({
+        message: 'La hora de inicio es requerida',
+    })
+    @IsString({
+        message: 'La hora de inicio debe ser un string',
+    })
+    @IsValidTime(FORMATS.TIME, {
+        message: `La hora de inicio debe tener el formato ${FORMATS.TIME}`,
+    })
+    public start_time: string
+
+    @IsNotEmpty({
+        message: 'La hora de fin es requerida',
+    })
+    @IsString({
+        message: 'La hora de fin debe ser un string',
+    })
+    @IsValidTime(FORMATS.TIME, {
+        message: `La hora de fin debe tener el formato ${FORMATS.TIME}`,
+    })
+    public end_time: string
+
+    @IsNotEmpty({
+        message: 'La hora de inicio de descanso es requerida',
+    })
+    @IsString({
+        message: 'La hora de inicio de descanso debe ser un string',
+    })
+    @IsValidTime(FORMATS.TIME, {
+        message: `La hora de inicio de descanso debe tener el formato ${FORMATS.TIME}`,
+    })
+    public start_rest_time: string
+
+    @IsNotEmpty({
+        message: 'La hora de fin de descanso es requerida',
+    })
+    @IsString({
+        message: 'La hora de fin de descanso debe ser un string',
+    })
+    @IsValidTime(FORMATS.TIME, {
+        message: `La hora de fin de descanso debe tener el formato ${FORMATS.TIME}`,
+    })
+    public end_rest_time: string
+
+    @IsNotEmpty({
+        message: 'El dia es requerido',
+    })
+    @IsNumber(
+        {},
+        {
+            message: 'El dia debe ser un numero',
+        },
+    )
+    @IsPositive({
+        message: 'El dia debe ser un numero positivo',
+    })
+    public day: number
+}
--- src/modules/schedules/entities/schedule.entity.ts
+++ src/modules/schedules/entities/schedule.entity.ts
@@ -1,1 +1,55 @@
-export class Schedule {}
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm'
+import { User } from 'src/modules/users/entities/user.entity'
+import { Exclude } from 'class-transformer'
+import { DateTime } from 'luxon'
+
+@Entity({
+    name: 'schedules',
+})
+export class Schedule {
+    @PrimaryColumn({
+        generated: 'increment',
+    })
+    public id: number
+
+    @Column({
+        nullable: true,
+    })
+    @Exclude()
+    public user_barber_id: number
+
+    @Column({
+        type: 'timestamp',
+    })
+    public start_time: DateTime
+
+    @Column({
+        type: 'timestamp',
+    })
+    public end_time: DateTime
+
+    @Column({
+        type: 'timestamp',
+    })
+    public start_rest_time: DateTime
+
+    @Column({
+        type: 'timestamp',
+    })
+    public end_rest_time: DateTime
+
+    @Column()
+    public day: number
+
+    @ManyToOne(() => User, (user) => user.schedules, {
+        eager: true,
+    })
+    @JoinColumn({
+        name: 'user_barber_id',
+    })
+    public user: User
+
+    constructor(partial: Partial<Schedule>) {
+        Object.assign(this, partial)
+    }
+}
